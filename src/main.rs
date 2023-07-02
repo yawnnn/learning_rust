6dd1ed42d6a1f09ae0dd655b11eb8a347c30f701
yawnnn <alessandromy7476@gmail.com>
2023-07-02 10:59:40 +0200

    2: roman numerals

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,66 @@ mod two_sum {
     }
 }
 
+// 2: Roman numerals to integer
+mod roman_numerals {
+    use std::iter::zip;
+    use RomanNumeral::*;
+
+    #[derive(PartialEq, Eq, Hash)]
+    enum RomanNumeral {
+        I, V, X, L, C, D, M,
+    }
+
+    impl TryFrom<char> for RomanNumeral {
+        type Error = ();
+
+        fn try_from(c: char) -> Result<Self, Self::Error> {
+            match c {
+                'I' | 'i' => Ok(I), 'V' | 'v' => Ok(V),
+                'X' | 'x' => Ok(X), 'L' | 'l' => Ok(L),
+                'C' | 'c' => Ok(C), 'D' | 'd' => Ok(D),
+                'M' | 'm' => Ok(M),
+                _ => Err(()),
+            }
+        }
+    }
+
+    impl From<RomanNumeral> for i32 {
+        fn from(rn: RomanNumeral) -> i32 {
+            match rn {
+                I => 1, V => 5,
+                X => 10, L => 50,
+                C => 100, D => 500,
+                M => 1000,
+            }
+        }
+    }
+
+    pub fn roman_to_integer(s: &str) -> i32 {
+        let numerals: Vec<i32> = s
+            .chars()
+            .filter_map(|c| RomanNumeral::try_from(c).ok())
+            .map(|numeral| numeral.into())
+            .collect();
+
+        let mut total = 0;
+
+        for (n1, n2) in zip(numerals.iter(), numerals.iter().skip(1)) {
+            if n2 > n1 {
+                total -= n1;
+            } else {
+                total += n1;
+            }
+        }
+
+        if let Some(n) = numerals.last() {
+            total += *n;
+        }
+
+        total
+    }
+}
+
 fn main() {
-    
+    println!("{}", roman_numerals::roman_to_integer("MCMXCIV"));
 }
